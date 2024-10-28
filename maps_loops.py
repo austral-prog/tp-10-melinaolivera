a9ebef382cd850c109a7a738fecd40b6e357e17b
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-10-28 19:59:34 -0300

    Update maps_loops.py

--- maps_loops.py
+++ maps_loops.py
@@ -1,22 +1,59 @@
 def find_max_value(dict):
-    pass
-
+    valor = -1
+    nombre = none
+    for key,value in dict.items():
+        if value > valor:
+            valor = value
+            nombre = key
+    return nombre
 
 def reverse_dict(dict):
-    pass
+    a = dict()
+    for key, value in dict.items():
+        if value in a:
+            a[value] += key # de esta manera lo doy vuelta y le sumo los valores iguales
+        else:
+            a[value] = key
+    return a
 
 
 def word_freq_counter(words):
-    pass
+    diccionario = dict()
+    for word in words:
+        if word in diccionario:
+            diccionario[word] += 1
+        else:
+            diccionario[word] = 1
+    return diccionario
 
 
 def find_biggest_expense(dict):
-    pass
-
+    valor = 0
+    gasto = none
+    for key, value in dict.items():
+        costo = sum(value) / len(value) # calculo el promedio
+        if costo > valor:
+            valor = costo
+            gasto = key
+    return gasto
 
 def sum_of_expenses(expenses):
-    pass
+    diccionario = dict()
+    for key, value in expenses.items():
+        suma = sum(value)
+        diccionario[key] = suma
+    return diccionario
+dictt = {'Food': [60, 80, 100], 'Transport': [10, 1, 2], 'Games': [10, 20, 30]}
+print(sum_of_expenses(dictt))
+
 
 
 def sum_of_expenses_by_type(expenses):
-    pass
+    diccionario = dict()
+    for key, value in expenses.items():
+        for type,cost in expenses:
+            if type in diccionario:
+                diccionario[type] += cost
+            else:
+                diccionario[type] = cost
+    return diccionario
