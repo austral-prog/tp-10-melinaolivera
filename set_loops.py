a93360eb2376410acb4de76ebecf16cc44a6055c
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-10-28 21:15:30 -0300

    Update set_loops.py

--- set_loops.py
+++ set_loops.py
@@ -1,2 +1,11 @@
 def unique_strings(words):
-    pass
+    unique_chars = set()  # Usar un conjunto para almacenar caracteres únicos
+    for char in words:
+        unique_chars.add(char)  # Agrega el caracter al conjunto
+    return unique_chars  # Devuelve el conjunto de caracteres únicos
+
+
+
+input_str = "hello"
+result = unique_strings(input_str)
+print(result)  
